72af9abdc7a02083eed2fddc8f3602c572c9b4d3
Samuel47535 <a47535@alunos.ipb.pt>
2022-11-06 11:41:24 +0000

    Etapa 3 - Accelaration

--- Tutorial1/Assets/Velocity_Accelaration/Scripts/Shell.cs
+++ Tutorial1/Assets/Velocity_Accelaration/Scripts/Shell.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Shell : MonoBehaviour
 {
     public GameObject explosion;
+    float mass = 10;
+    float force = 600;
+    float accelaration;
+    float gravity = -9.8f;
+    float gAccel;
+    float speedZ;
+    float speedY;
 
     void OnCollisionEnter(Collision col)
     {
@@ -25,6 +32,12 @@ public class Shell : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        accelaration = force / mass;
+        speedZ += accelaration * Time.deltaTime;
+        gAccel = gravity / mass;
+        speedY += gAccel * Time.deltaTime;
+        this.transform.Translate(0, speedY, speedZ);
 
+        force = 0;
     }
 }
